533b25772fed48fa43826fc5093c365d28c231aa
Adel Sharifat <a.sh.3287@gmail.com>
2022-01-16 17:03:04 +0330

    bugfix pwa support better

--- Frontend/ProjectProgressApp/src/main.ts
+++ Frontend/ProjectProgressApp/src/main.ts
@@ -8,9 +8,4 @@ if (environment.production) {
   enableProdMode();
 }
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .then(()=>{
-    if ('serviceWorker' in navigator && environment.production) {
-      navigator.serviceWorker.register('/ngsw-worker.js');
-    }
-  })
   .catch(err => console.error(err));
